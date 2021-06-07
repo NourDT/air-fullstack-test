e6c3196158cc660c77948e86e12eb32fe35e3a88
wloonteh <32573853+wloonteh@users.noreply.github.com>
2021-06-08 00:24:06 +0800

    update environments

--- appointment/src/environments/environment.ts
+++ appointment/src/environments/environment.ts
@@ -5,6 +5,15 @@
 export const environment = {
   production: false,
   api_url: 'https://oxst6ge78f.execute-api.ap-southeast-1.amazonaws.com/dev',
+  api_key: 'cxp3uSUfkx6i6MVAohlIV85GfsnlcVsI6aK0Ltb5',
+  awsconfig: {
+    aws_project_region: 'ap-southeast-1',
+    aws_appsync_graphqlEndpoint:
+      'https://vdpj3nkxw5eknhbbrv34hgmlxa.appsync-api.ap-southeast-1.amazonaws.com/graphql',
+    aws_appsync_region: 'ap-southeast-1',
+    aws_appsync_authenticationType: 'API_KEY',
+    aws_appsync_apiKey: 'da2-4uxtixgpbfgcbkc5uzoyuxfxia',
+  },
 };
 
 /*

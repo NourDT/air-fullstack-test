38be88818e6be71a4fc6668f306ff489e648415c
Nicholas Cheng <nchengyeeshen@gmail.com>
2021-07-13 22:57:00 +0800

    Add Calendar component
    Integrate DatePicker component to become Calendar component. This commit
    also fixes a bug in DatePicker when month == 0.

---
+++ src/Calendar.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+/**
+ * Date picker component.
+ *
+ * The month is zero-indexed. E.g. January is 0, February is 1 and so on.
+ */
+const DatePicker = ({ month, year, onClick }) => {
+  // 0 (Sun) - 6 (Sat)
+  const startDay = new Date(year, month, 1).getDay();
+  // Setting day argument to 0 get the date from 1 day before
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  // Pad the front of the array to get first day in the correct column
+  const days = Array.prototype.concat(
+    Array(startDay).fill(null),
+    Array(daysInMonth)
+      .fill(0)
+      .map((_, index) => index + 1)
+  );
+
+  return (
+    <div className="grid grid-cols-7 gap-4">
+      {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((value) => (
+        <p key={`day-${value}`}>{value}</p>
+      ))}
+      {days.map((elem, index) => {
+        if (elem) {
+          return (
+            <button
+              key={`item-${index}`}
+              onClick={onClick}
+              className="border w-6 h-6 rounded-full flex items-center justify-center p-4"
+            >
+              {elem}
+            </button>
+          );
+        } else {
+          return <span key={`item-${index}`}></span>;
+        }
+      })}
+    </div>
+  );
+};
+
+export class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const currDate = new Date();
+    this.state = {
+      month: currDate.getUTCMonth(),
+      year: currDate.getUTCFullYear(),
+      min: `${currDate.getUTCFullYear()}-${String(
+        currDate.getMonth() + 1
+      ).padStart(2, "0")}`,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <input
+          type="month"
+          value={`${this.state.year}-${String(this.state.month + 1).padStart(
+            2,
+            "0"
+          )}`}
+          min={this.state.min}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <DatePicker month={this.state.month} year={this.state.year} />
+      </>
+    );
+  }
+
+  handleChange(event) {
+    const value = new Date(event.target.value);
+    this.setState({
+      month: value.getUTCMonth(),
+      year: value.getUTCFullYear(),
+    });
+  }
+}

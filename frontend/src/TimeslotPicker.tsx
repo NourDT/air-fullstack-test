48c2dd5683e64a90ac72cb689bd98866addd9c12
Nicholas Cheng <nchengyeeshen@gmail.com>
2021-07-18 10:04:10 +0800

    Display no timeslots

--- frontend/src/TimeslotPicker.tsx
+++ frontend/src/TimeslotPicker.tsx
@@ -9,14 +9,18 @@ export const TimeslotPicker = (props: TimeslotPickerProps) => (
   <div>
     <h2 className="text-lg">{props.date.format("dddd D MMMM YYYY")}</h2>
     <div className="h-96 overflow-y-auto">
-      {props.slots.map((value) => (
-        <button
-          key={value.valueOf()}
-          className="block border min-w-full p-4 mt-4 rounded-lg hover:bg-blue-400 hover:text-gray-100"
-        >
-          {value.format("hh:mma")}
-        </button>
-      ))}
+      {props.slots.length === 0 ? (
+        <p className="font-medium mt-4">No available timeslots</p>
+      ) : (
+        props.slots.map((value) => (
+          <button
+            key={value.valueOf()}
+            className="block border min-w-full p-4 mt-4 rounded-lg hover:bg-blue-400 hover:text-gray-100"
+          >
+            {value.format("hh:mma")}
+          </button>
+        ))
+      )}
     </div>
   </div>
 );
